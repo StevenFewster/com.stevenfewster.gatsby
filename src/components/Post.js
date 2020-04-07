96e51d5ce54277d9ab3846dfb94e2f308191c60b
Steven Fewster <stevenfewster@Stevens-MacBook-Pro.local>
2020-04-07 11:54:16 +0100

    adding imagery and posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,11 +4,11 @@ import Img from "gatsby-image"
 import "./post.css"
 
 const Post = ({ title, author, date, description, path, cover }) => (
-  <div className="post w-100 m-4 py-2 bg-white shadow-lg">
+  <div className="post w-100 m-4 py-2 bg-white">
     <div className="post-image">
       <Img fluid={cover} />
     </div>
-    <div className="post-details">
+    <div className="post-details shadow-lg">
       <h3 className="post-title">{title}</h3>
       <p className="post-description">{description}</p>
       <p className="post-written-by">
